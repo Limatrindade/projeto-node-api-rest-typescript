b2edd0df73fb5f449eaf507390c08bbc48618a0e
Matheus Lima <teulima20@hotmail.com>
2023-07-03 17:33:09 -0300

    feat: added code styles configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
-import express from "express"
+import express from 'express';
 
-const server = express()
+const server = express();
 
-server.get("/", (_, res) => {
-res.send("ola dev")
-})
+server.get('/', (_, res) => {
+  res.send('ola dev');
+});
 
-export { server }
+export { server };
