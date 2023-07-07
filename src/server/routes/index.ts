e776ce2be429a2d8a83d82779670b79c8f137fb1
Matheus Lima <teulima20@hotmail.com>
2023-07-07 19:47:44 -0300

    feat: added function to create validation middleware

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,6 +8,7 @@ router.get('/', (_, res) => {
   return res.send('Olá, DEV!');
 });
 
-router.post('/cidades', CidadesController.create);
+router.post('/cidades', CidadesController.createValidation, CidadesController.create
+);
 
 export { router };
